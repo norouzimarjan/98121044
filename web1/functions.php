6f5b567a55f0c1ba918ece6da4e0482bff67c4fa
marjan norouzi <marjanma01@gmail.com>
2023-10-27 15:47:30 +0330

    session2

--- web1/functions.php
+++ web1/functions.php
@@ -5,3 +5,32 @@ function dd($a)
   var_dump($a);
   die;
 }
+function connect_to_mysql()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  // Create connection
+  $connection = new mysqli($servername, $username, $password);
+
+  // Check connection
+  if ($connection->connect_error) {
+    return false;
+  }
+  return $connection;
+
+}
+function get_lessons_in_this_time($profs_lessons, $lessons, $profs ,$k)
+{
+  $cell = "";
+  foreach($profs_lessons as $prof_id => $lesson_ids){
+    foreach($lesson_ids as $lesson_id){
+        $program = $lessons[$lesson_id]['program'];
+        if(in_array($k,$program)){
+            $cell .= $lessons[$lesson_id]['title'].' ('.$profs[$prof_id]['name'].')';
+        } 
+    }
+  }
+  return $cell;
+}
